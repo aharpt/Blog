d75e0410f4224bda2b4914ee34c428541f86b674
aaron.harpt <aaron.harpt@gmail.com>
2016-11-16 12:50:29 -0600

    Added code to app.js file

---
+++ src/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var express = require("express"),
+    posts = require("./mock/posts.json")
+
+var app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/templates");
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/blog/:title?", function(req, res) {
+  var title = req.params.title;
+  if (title === undefined) {
+    res.status(503);
+    res.send("This page is under construction.");
+  } else {
+    var post = posts[title] || {};
+    res.render("post", {post: post});
+  }
+});
+
+app.listen(3000, function() {
+  console.log("Frontend server running at port 3000");
+});
