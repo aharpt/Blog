b782dbecc2d3709de963a89f6c90d22dc91f5d41
aaron.harpt <aaron.harpt@gmail.com>
2016-11-16 11:47:50 -0600

    Required and wrote task for gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var cleanCSS = require('gulp-clean-css');
 
 /* Compile Sass */
 
 gulp.task("sass", function(){
   return gulp.src("./**/.scss");
   .pipe(gulp.dest("./css"));
+});
+
+gulp.task("minify-css", function() {
+  return gulp.src("./css");
+  .pipe(gulp.dest("dist"))
 });
