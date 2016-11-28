4acfb53cdcfd51d9d21df0f89c9ab0ac4cd79776
aaron.harpt <aaron.harpt@gmail.com>
2016-11-28 17:49:46 -0600

    Worked on site

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -15,18 +15,20 @@
 
 // .form-group button
 
-$(".form-group button").click(function() {
-  if ($("#name").length < 2) {
-    $("#name").parent(".form-group").addClass("error-top").append("<p class='text-danger'>Your Name is required</p>");
-    // console.log($("#name").length);
-  }
-});
-
-
-$("#name").keyup(function() {
-  if ($("input#name").length > 1) {
-    console.log($("input#name").length);
-    $(this).parent(".form-group").removeClass("error-top");
-    $(this).children(".text-danger").hide();
-  }
-});
+// $(".form-group button").click(function() {
+//   if ($("#name").length < 2) {
+//     $("#name").parent(".form-group").addClass("error-top").append("<p class='text-danger'>Your Name is required</p>");
+//     // console.log($("#name").length);
+//   }
+// });
+//
+//
+// $("#name").keyup(function() {
+//   $(this).parent(".form-group").removeClass("error-top");
+//   $(".text-danger").hide();
+//   if ($("input#name").length > 1) {
+//     console.log($("input#name").length);
+//     $(this).parent(".form-group").removeClass("error-top");
+//     $(this).children(".text-danger").hide();
+//   }
+// });
